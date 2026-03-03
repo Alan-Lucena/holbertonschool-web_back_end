3a92b40a503c125a160e192e233f98f3ad641ff7
Alan <50720520+Alan-Lucena@users.noreply.github.com>
2026-03-03 17:37:51 -0600

    Fix 12-log_stats.py - use f-strings

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB
+"""Provides stats about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
 
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     nginx_collection = client.logs.nginx
 
     num_logs = nginx_collection.count_documents({})
-    print("{} logs".format(num_logs))
+    print(f"{num_logs} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        print(f"\tmethod {method}: {count}")
 
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print("{} status check".format(status_check))
+    print(f"{status_check} status check")
