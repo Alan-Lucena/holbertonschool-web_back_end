b261b1e8c1609932b524079fb60f7c7c61d20f7c
Alan <50720520+Alan-Lucena@users.noreply.github.com>
2026-03-03 17:39:08 -0600

    Fix 12-log_stats.py with main function and docstrings

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB.
+"""Provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def main():
+    """Main function to display stats
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    db = client.logs
+    collection = db.nginx
 
-    num_logs = nginx_collection.count_documents({})
-    print(f"{num_logs} logs")
-
+    print("{} logs".format(collection.count_documents({})))
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})))
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    main()
