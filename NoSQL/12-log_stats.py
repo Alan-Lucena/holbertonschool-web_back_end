08e148a0579c5e1f108c4d83151edf1a53352ba2
Alan <50720520+Alan-Lucena@users.noreply.github.com>
2026-03-03 17:36:25 -0600

    Fix 12-log_stats.py - simplify structure

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Script that provides stats about Nginx logs stored in MongoDB"""
+"""Provides stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Provides stats about Nginx logs"""
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
@@ -20,7 +20,3 @@ def log_stats():
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
     print("{} status check".format(status_check))
-
-
-if __name__ == "__main__":
-    log_stats()
